6c7ac5179c5b13e7c6ca6770b184c39cfe093cac
Ryan <roche189@umn.edu>
2024-02-29 12:24:31 -0600

    Implemented GraphNode Model

---
+++ gophermaps-ios/Models/GraphNode.swift
@@ -0,0 +1,13 @@
+//
+//  GraphNode.swift
+//  gophermaps-ios
+//
+//  Created by Ryan Roche on 2/29/24.
+//
+
+import SwiftUI
+
+struct GraphNode {
+    let nodeID: String
+    let building: BuildingEntry
+}
