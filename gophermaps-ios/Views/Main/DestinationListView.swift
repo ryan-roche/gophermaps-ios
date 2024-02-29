c220420b94c61e696d95763264a5559ecbd87ad4
Ryan <roche189@umn.edu>
2024-02-28 18:45:16 -0600

    Added outlines for StartListView, DestinationListView, and RoutePreviewView

---
+++ gophermaps-ios/Views/Main/DestinationListView.swift
@@ -0,0 +1,22 @@
+//
+//  DestinationListView.swift
+//  gophermaps-ios
+//
+//  Created by Ryan Roche on 2/28/24.
+//
+
+import SwiftUI
+
+struct DestinationListView: View {
+    var body: some View {
+        Text("Destination List View")
+        
+        // Takes a BuildingEntry object
+        // Lists the BuildingEntry objects in the destination set of the passed BuildingEntry in alphabetical order
+        // Clicking one navigates to a RoutePreviewView with the start and destination BuildingEntry objects passed to it
+    }
+}
+
+#Preview {
+    DestinationListView()
+}
---
+++ gophermaps-ios/Views/Main/StartListView.swift
@@ -0,0 +1,22 @@
+//
+//  StartListView.swift
+//  gophermaps-ios
+//
+//  Created by Ryan Roche on 2/28/24.
+//
+
+import SwiftUI
+
+struct StartListView: View {
+    var body: some View {
+        Text("Start List View")
+        
+        // Takes the list of available starting points
+        // List each one as a BuildingCard, in alphabetic order
+        // Clicking one takes you to a DestinationListView created using that BuildingEntry as a parameter
+    }
+}
+
+#Preview {
+    StartListView()
+}
