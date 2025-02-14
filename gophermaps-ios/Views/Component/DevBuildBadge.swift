f3fb6018723d788e6b7fe14b67f7884c5f7efb89
Ryan <roche189@umn.edu>
2025-02-14 13:57:23 -0600

    Add "Development Build" badge

--- gophermaps-ios/Views/Component/DevBuildBadge.swift
+++ gophermaps-ios/Views/Component/DevBuildBadge.swift
@@ -4,15 +4,22 @@
 //
 //  Created by Ryan Roche on 2/14/25.
 //
-
+#if DEBUG
 import SwiftUI
 
 struct DevBuildBadge: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Label("Dev Build", systemImage: "hammer.fill")
+            .padding()
+            .background(
+                FrostedGlassView(effect: .systemChromeMaterial)
+                    .clipShape(Capsule(style:.circular))
+                    .shadow(color:.black.opacity(0.25), radius: 4, y:2)
+            )
     }
 }
 
 #Preview {
     DevBuildBadge()
 }
+#endif
