e9af9fdc71983d47b29a058f697c9559649ca258
Ryan <roche189@umn.edu>
2025-02-14 13:57:13 -0600

    Remove automatic bumping of build number- caused infinite loop when compiling

---
+++ gophermaps-ios/Views/Component/DevBuildBadge.swift
@@ -0,0 +1,18 @@
+//
+//  DevBuildBadge.swift
+//  gophermaps-ios
+//
+//  Created by Ryan Roche on 2/14/25.
+//
+
+import SwiftUI
+
+struct DevBuildBadge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DevBuildBadge()
+}
