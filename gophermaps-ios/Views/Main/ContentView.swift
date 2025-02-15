f8dc5a6bddf245b6fe505379637edb55a2514f37
Ryan <roche189@umn.edu>
2025-02-15 17:58:09 -0600

    add dev build badge to main view

--- gophermaps-ios/Views/Main/ContentView.swift
+++ gophermaps-ios/Views/Main/ContentView.swift
@@ -11,6 +11,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         MapView()
+        #if DEBUG
+            .overlay(alignment:.top) {
+                DevBuildBadge()
+                    .padding()
+                    .background(FrostedGlassView(effect: .systemThickMaterial))
+                    .clipShape(Capsule())
+            }
+        #endif
     }
 }
 
