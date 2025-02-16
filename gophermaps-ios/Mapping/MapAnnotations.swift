1ccc21f632d7466fe42519fed2b830c40fc64fb1
Ryan <roche189@umn.edu>
2025-02-15 20:26:44 -0600

    Move map annotation loader functions to their own file

--- gophermaps-ios/Mapping/MapAnnotations.swift
+++ gophermaps-ios/Mapping/MapAnnotations.swift
@@ -8,11 +8,52 @@
 import Foundation
 import MapKit
 
-actor BuildingLoader {
-    static let shared = BuildingLoader()
-    
-    init() {
-        // Begin loading buildings as soon as object is initialized- from API for prod, from file for Debug
-        
-    }
+func loadBuildingMarkers(area: Components.Schemas.AreaModel) {
+    return
 }
+
+
+// MARK: Debug functions
+
+#if DEBUG
+enum RegionBoundsError: Error {
+    case fileNotFound
+    case invalidData
+    case decodingError
+}
+
+func loadRegionBounds() throws -> [MKPolygon] {
+    guard let url = Bundle.main.url(forResource: "areaBounds", withExtension: "geojson") else {
+        throw RegionBoundsError.fileNotFound
+    }
+    
+    let data: Data
+    do {
+        data = try Data(contentsOf: url)
+    } catch {
+        throw RegionBoundsError.invalidData
+    }
+    
+    let decoder = MKGeoJSONDecoder()
+    let gjobjects: [Any]
+    do {
+        gjobjects = try decoder.decode(data)
+    } catch {
+        throw RegionBoundsError.decodingError
+    }
+    
+    var newPolys: [MKPolygon] = []
+    for object in gjobjects {
+        guard let feature = object as? MKGeoJSONFeature else {
+            continue
+        }
+        for geom in feature.geometry {
+            if let polygon = geom as? MKPolygon {
+                newPolys.append(polygon)
+            }
+        }
+    }
+    
+    return newPolys
+}
+#endif
